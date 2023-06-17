01aae0c1a390d5dc6b229a68ab2e025e40ece894
Victor Berchet <victor@suumit.com>
2023-06-17 16:27:04 -0700

    feat: add benchmark examples (#678)

---
+++ examples/bench-advanced.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { MAP_ID, getLoaderOptions } from "./config";
+import { Loader } from "@googlemaps/js-api-loader";
+import { MarkerClusterer } from "../src";
+import points from "./realworld.json";
+
+// Do not set the mapId to force legacy markers
+const mapOptions: google.maps.MapOptions = {
+  center: { lat: -37.89, lng: 175.46 },
+  zoom: 8,
+  maxZoom: 18,
+  mapId: MAP_ID,
+};
+
+new Loader(getLoaderOptions()).load().then(() => {
+  const element = document.getElementById("map");
+
+  const map = new google.maps.Map(element, mapOptions);
+
+  const markers = (points as [number, number, string][]).map(
+    ([lat, lng, title]) =>
+      new google.maps.marker.AdvancedMarkerElement({
+        position: { lat, lng },
+        map,
+        title,
+      })
+  );
+
+  const markerCluster = new MarkerClusterer({
+    markers,
+    algorithmOptions: { maxZoom: 30 },
+  });
+
+  markerCluster.setMap(map);
+});
