c3c0544c7c03bbd24dad3b83f2d5f14fa772e32e
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-12-06 11:06:56 +0100

    chore: remaining dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -16,7 +16,7 @@
 
 import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
