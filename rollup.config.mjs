119a90779aff5566c06f9117de9a0c3f11648a39
Martin Schuhfuss <m.schuhfuss@gmail.com>
2025-07-06 17:01:36 +0200

    fix: dependency update (#1006)
    - updated jest to v30 and handled deprecations
    - update typescript to latest version and add tslib
    - moved @typs/supercluster from devDependencies to dependencies, should fix problems with the reexported types (see #724)
    - fix externals for esm build (only cosmetic difference)
    - add permissions for docs workflow
    
    fixes #724

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -68,7 +68,7 @@ export default [
   },
   {
     input: "src/index.ts",
-    external: ["supercluster", "fast-deep-equal"],
+    external: ["supercluster", "fast-equals"],
     plugins: [typescript({ tsconfig: "./tsconfig.json" })],
     output: {
       file: "dist/index.esm.mjs",
